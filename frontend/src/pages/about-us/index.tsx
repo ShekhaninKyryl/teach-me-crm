6e81c94d5de3e80327729b6d09dd51de3811c105
ShekhaninKyryl <kyryl.shekhanin@outlook.com>
2026-02-17 15:12:39 +0200

    [f-bug-report]: add Bugreport page, fix about us, payments pages;

---
+++ frontend/src/pages/about-us/index.tsx
@@ -0,0 +1,9 @@
+import { UnderDevelopment } from "components/under-development";
+
+export const AboutUsPage = () => {
+  return (
+    <div className="p-4">
+      <UnderDevelopment />
+    </div>
+  );
+};
